6ca0caeae6fdd5b803adf0aa6c63d9f0479aff78
Mikael Ramdeen <mikael.ramdeen@hotmail.com>
2022-11-23 15:35:16 -0400

    .

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,9 +29,8 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html lang='en-GB'>
+      <Html lang='en'>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
           <meta charSet='utf-8'/>
           <meta name="description" content="Mikael Ramdeen dev portfolio."></meta>
           <title>Mikael Ramdeen</title>
